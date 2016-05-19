6c8cbf7a003dd6183ed771490e429f913aebe6a1
Daniwalls97 <chocodanicadbury@gmail.com>
2016-05-19 11:17:14 +0100

    Jajajajaja

--- src/Yosise.java
+++ src/Yosise.java
@@ -4,6 +4,8 @@ public class Yosise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Que tal te va ahora con eso?");
+		
+		System.out.println("Aqui el PRO soy yo ! ");
 	}
 
 }
